13b329faabb8461729ee8cbbe4a8db34e788dca2
630815708 <630815708@qq.com>
2017-09-12 22:08:22 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdemc/android/mobileguard/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdemc.android.mobileguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        System.out.print("Hello world!!");
+    }
+}
